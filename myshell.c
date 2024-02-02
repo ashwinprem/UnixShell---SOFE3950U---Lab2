0a6b3eeb80ec3cb1e9eadaa17dcd6a4d82f81879
Neuron-Chan <118234475+Neuron-Chan@users.noreply.github.com>
2024-02-02 18:27:52 -0500

    Update myshell.c

--- myshell.c
+++ myshell.c
@@ -1,9 +1,13 @@
 #include "myshell.h"
+
+//Takes a single argument, input, which is a pointer to a character array (string). This function is responsible for parsing the input command and executing it accordingly.
 void execute_command(char *input) {
     char *token;
     char *delim = " \n";
     token = strtok(input, delim);
 
+    /* The first token is checked to see if it's a recognized command, then it attempts to get the next token, (directory path), 
+    and calls change_directory to change the current working directory. Prints error otherwise. */
     if (token != NULL) {
         if (strcmp(token, "cd") == 0) {
             token = strtok(NULL, delim);
