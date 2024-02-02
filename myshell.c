4f222b72255144ec5dc2ff2a93ea4b8ae72cd344
ashwinprem <ashwin.prem@ontariotechu.net>
2024-02-02 18:41:36 -0500

    Implemented 'cd' command in shell.

---
+++ myshell.c
@@ -0,0 +1,34 @@
+#include "myshell.h"
+
+void execute_command(char *input)
+{
+  char *token;
+  char *delim = "\n";
+  token = strtok (input, delim);
+
+  if (token != NULL) 
+  {
+    if (strcmp(token, "cd") == 0) 
+    {
+      token = strtok(NULL, delim); // Get next part (directory path)
+      change_directory(token);
+    } else {
+      printf("Command not recognized.\n");
+    }
+  }
+}
+
+int main()
+{
+  char input[1024]; // Buffer for user input
+  while (1)
+  {
+    printf("MyShell> ");
+    if (fgets(input, sizeof(input), stdin) == NULL)
+    {
+      break; // Exit on EOF
+    }
+    execute_command(input); // Process the input
+  }
+  return 0;
+}
---
+++ myshell.h
@@ -0,0 +1,9 @@
+#ifndef MYSHELL_H
+#define MYSHELL_H
+#include <stdio.h> // Standard I/O functions
+#include <stdlib.h> // Standard library for functions like malloc
+#include <string.h> // String handling functions
+#include <unistd.h> // Provides access to the POSIX operating system API
+
+void change_directory(char *path); // declaration of 'cd' function (cd function prototype)
+#endif
