a911a19e647bfe820ac91755fb169e5ab6eb2925
ashwinprem <ashwin.prem@ontariotechu.net>
2024-02-09 18:51:07 -0500

    Added all the functionalities, along with two new files: batchfile and user_manual.txt.

--- myshell.h
+++ myshell.h
@@ -4,6 +4,9 @@
 #include <stdlib.h> // Standard library for functions like malloc
 #include <string.h> // String handling functions
 #include <unistd.h> // Provides access to the POSIX operating system API
-
+void list_directory_contents(char *path);
+void list_environment_strings();
+void pause_shell();
 void change_directory(char *path); // declaration of 'cd' function (cd function prototype)
+void echo_input(char *input); // Declare echo_input function
 #endif
