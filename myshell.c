9059f067d18dba138c63643806896e9e265d4e43
T-Ginger-J <trent.jordan@ontariotechu.net>
2024-02-10 13:29:14 -0500

    moving functions to utility.c for consistency

--- myshell.c
+++ myshell.c
@@ -1,7 +1,4 @@
 #include "myshell.h"
-#include <dirent.h> // Directory entry
-#include <errno.h> // Error number definitions
-#include <ctype.h> // For isspace()
 
 #define RESET "\x1b[0m"
 #define ERROR "\x1b[31m"
@@ -10,9 +7,6 @@
 
 
 void execute_command(char *input);
-void set_shell_environment_variable();
-void execute_external_command(char *input);
-void execute_batch_mode(char *filename);
 
 int main(int argc, char *argv[])
 {
@@ -28,7 +22,7 @@ int main(int argc, char *argv[])
     while (1)
     {
     	getcwd(input, sizeof(input));
-        printf(PURP "MyShell> " CYAN "%s>" RESET, input);
+        printf(PURP "MyShell> " CYAN "%s> " RESET, input);
         if (fgets(input, sizeof(input), stdin) == NULL)
         {
             break; // Exit on EOF or error
@@ -69,114 +63,6 @@ void execute_command(char *input)
     }
 }
 
-void execute_external_command(char *input)
-{
-    pid_t pid = fork();
-    if (pid == 0) {
-        // Child process
-        char *envp[] = { NULL }; // Setup environment if necessary
-        char *argv[] = { "/bin/sh", "-c", input, NULL };
-        if (execve("/bin/sh", argv, envp) == -1) { 
-            perror("myshell");
-            exit(EXIT_FAILURE);
-        }
-    } else if (pid > 0) {
-        // Parent process
-        wait(NULL); // Wait for the child process to finish
-    } else {
-        // Failed to fork
-        perror("myshell");
-    }
-}
-
-void execute_batch_mode(char *filename)
-{
-    FILE *file = fopen(filename, "r");
-    if (!file) {
-        perror("myshell");
-        exit(EXIT_FAILURE);
-    }
-
-    char line[1024];
-    while (fgets(line, sizeof(line), file) != NULL) {
-        execute_command(line);
-    }
-
-    fclose(file);
-}
-
-void set_shell_environment_variable()
-{
-    char cwd[1024];
-    if (getcwd(cwd, sizeof(cwd)) != NULL) {
-        char shell_env[1024];
-        sprintf(shell_env, "SHELL=%s/myshell", cwd);
-        putenv(shell_env);
-    } else {
-        perror("getcwd() error");
-    }
-}
-
-void list_directory_contents(char *path)
-{
-    DIR *d;
-    struct dirent *dir;
-    if (path == NULL) {
-        char cwd[1024];
-        if (getcwd(cwd, sizeof(cwd)) != NULL) {
-            path = cwd;
-        } else {
-            perror("getcwd() error");
-            return;
-        }
-    }
-    d = opendir(path);
-    if (d) {
-        while ((dir = readdir(d)) != NULL) {
-            printf("%s\n", dir->d_name);
-        }
-        closedir(d);
-    } else {
-        perror("opendir() error");
-    }
-}
-
-extern char **environ; // Environment variables
-
-void list_environment_strings()
-{
-    for (char **env = environ; *env; env++) {
-        printf("%s\n", *env);
-    }
-}
-
-void pause_shell()
-{
-    printf("Press 'Enter' to continue...\n");
-    while (getchar() != '\n');
-    // Clear the input buffer in case of additional characters before the newline
-    while (getchar() != '\n' && !feof(stdin) && !ferror(stdin));
-}
-
-void echo_input(char *input)
-{
-    int in_space = 1; // Start assuming we're leading with spaces
-    while (*input) {
-        if (isspace((unsigned char)*input)) {
-            // If we encounter a space and it's the first one after a word
-            if (!in_space) {
-                putchar(' '); // Print a single space
-                in_space = 1; // and mark that we're now in a space
-            }
-        } else {
-            // If it's not a space, print the character
-            putchar(*input);
-            in_space = 0; // and mark that we're not in a space
-        }
-        input++;
-    }
-    putchar('\n'); // Print a newline at the end
-}
 
 
 /*#include "myshell.h"
--- utility.c
+++ utility.c
@@ -3,6 +3,9 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <dirent.h> // Directory entry
+#include <errno.h> // Error number definitions
+#include <ctype.h> // For isspace()
 
 void change_directory(char *path)
 {
@@ -20,4 +23,113 @@ void change_directory(char *path)
       perror("myshell");
     }
   }
+}
+
+void execute_external_command(char *input)
+{
+    pid_t pid = fork();
+    if (pid == 0) {
+        // Child process
+        char *envp[] = { NULL }; // Setup environment if necessary
+        char *argv[] = { "/bin/sh", "-c", input, NULL };
+        if (execve("/bin/sh", argv, envp) == -1) { 
+            perror("myshell");
+            exit(EXIT_FAILURE);
+        }
+    } else if (pid > 0) {
+        // Parent process
+        wait(NULL); // Wait for the child process to finish
+    } else {
+        // Failed to fork
+        perror("myshell");
+    }
+}
+
+void execute_batch_mode(char *filename)
+{
+    FILE *file = fopen(filename, "r");
+    if (!file) {
+        perror("myshell");
+        exit(EXIT_FAILURE);
+    }
+
+    char line[1024];
+    while (fgets(line, sizeof(line), file) != NULL) {
+        execute_command(line);
+    }
+
+    fclose(file);
+}
+
+void set_shell_environment_variable()
+{
+    char cwd[1024];
+    if (getcwd(cwd, sizeof(cwd)) != NULL) {
+        char shell_env[1024];
+        sprintf(shell_env, "SHELL=%s/myshell", cwd);
+        putenv(shell_env);
+    } else {
+        perror("getcwd() error");
+    }
+}
+
+void list_directory_contents(char *path)
+{
+    DIR *d;
+    struct dirent *dir;
+    if (path == NULL) {
+        char cwd[1024];
+        if (getcwd(cwd, sizeof(cwd)) != NULL) {
+            path = cwd;
+        } else {
+            perror("getcwd() error");
+            return;
+        }
+    }
+    d = opendir(path);
+    if (d) {
+        while ((dir = readdir(d)) != NULL) {
+            printf("%s\n", dir->d_name);
+        }
+        closedir(d);
+    } else {
+        perror("opendir() error");
+    }
+}
+
+extern char **environ; // Environment variables
+
+void list_environment_strings()
+{
+    for (char **env = environ; *env; env++) {
+        printf("%s\n", *env);
+    }
+}
+
+void pause_shell()
+{
+    printf("Press 'Enter' to continue...\n");
+    while (getchar() != '\n');
+    // Clear the input buffer in case of additional characters before the newline
+    while (getchar() != '\n' && !feof(stdin) && !ferror(stdin));
+}
+
+void echo_input(char *input)
+{
+    int in_space = 1; // Start assuming we're leading with spaces
+    while (*input) {
+        if (isspace((unsigned char)*input)) {
+            // If we encounter a space and it's the first one after a word
+            if (!in_space) {
+                putchar(' '); // Print a single space
+                in_space = 1; // and mark that we're now in a space
+            }
+        } else {
+            // If it's not a space, print the character
+            putchar(*input);
+            in_space = 0; // and mark that we're not in a space
+        }
+        input++;
+    }
+    putchar('\n'); // Print a newline at the end
 }
